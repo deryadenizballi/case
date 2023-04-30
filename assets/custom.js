921aadea858f4a248b0bd60e335f2e7346511f27
Derya Deniz Ballı <49809450+deryadenizballi@users.noreply.github.com>
2023-04-30 16:11:33 +0300

    the header is completed

--- assets/custom.js
+++ assets/custom.js
@@ -2,9 +2,26 @@ const menuBtn =document.querySelector(".header__menu__btn");
 const sidebar =document.querySelector(".sidebar");
 
 
-
+// Sidebar 
 menuBtn.addEventListener("click",()=>{
     menuBtn.classList.toggle("active");
     sidebar.classList.toggle("active");
 })
-console.log(menuBtn);
+
+// Slider 
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+// Slider tab 
+  function openCity(phoneName) {
+    var i;
+    var x = document.getElementsByClassName("phone");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    document.getElementById(phoneName).style.display = "block";
+  }
